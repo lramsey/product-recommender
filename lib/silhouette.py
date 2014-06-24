16c99796fa0ba9898c37c5c6e211af191e260041
lramsey <lramsey177@gmail.com>
2014-06-23 18:45:19 -0700

    make silhouette 'a' value more rigorously defined and update package.json version

--- lib/silhouette.py
+++ lib/silhouette.py
@@ -23,20 +23,19 @@ def averageSilhouettes(clusters, matrix):
     __init__(matrix)
     silhouettes = 0.0
     for i in range(0, len(clusters)):
-        s = silhouette(clusters[i])
+        s = silhouette(clusters[i], i)
         silhouettes += s
     return silhouettes/len(clusters)
 
-def silhouette(cluster):
-    a = 1.0
-    b = 1.0
+def silhouette(cluster, index):
+    a = distFromCentroid(cluster, cl.centroidList[index])
+    b = a.size
     for i in range(0, len(cl.centroidList)):
-        c = distFromCentroid(cluster, cl.centroidList[i])
-        if c < a:
-            b = a
-            a = c
-        elif c < b:
-            b = c
+        if i == index:
+            continue
+        dist = distFromCentroid(cluster, cl.centroidList[i])
+        if dist < b:
+            b = dist
         if b == 0:
             return 0
     sil = (b-a)/max(a,b)
