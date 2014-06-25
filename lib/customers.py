change init.py logic for parameter, fix distance formula, refine silhouettes, refactor run.py, and begin code cleanup

lib/customers.py
@@ -1,7 +1,7 @@
 import products as p
 import util     as u
 import numpy    as np
-import math
+# import math
 
 customers = []
 customersMap = {}
@@ -23,28 +23,28 @@ class Customer(object):
         self.purchases[item] = prodCount
         self.purchasesArr[p.productsMap[item]] = prodCount
 
-def customerSim(person1, person2):
-    one = person1.purchases
-    two = person2.purchases
-    intersection = 0.0
-    union = 0.0
-    for i in one:
-        if one[i] > 0 and two[i] > 0:
-            union += 1
-            intersection += 1;
-        elif one[i] > 0 or two[i] > 0:
-            union += 1
+# def customerSim(person1, person2):
+#     one = person1.purchases
+#     two = person2.purchases
+#     intersection = 0.0
+#     union = 0.0
+#     for i in one:
+#         if one[i] > 0 and two[i] > 0:
+#             union += 1
+#             intersection += 1;
+#         elif one[i] > 0 or two[i] > 0:
+#             union += 1
             
-    return math.tanh(math.sqrt(union - intersection))
+#     return math.tanh(math.sqrt(union - intersection))
 
-def customerMatrixiser():
-    customerMatrix = []
-    for i in range(0, len(customers)):
-        sims = []
-        for j in range(0,len(customers)):
-            sims.append(customerSim(customers[i], customers[j]))
-        customerMatrix.append(sims)
-    return np.array(customerMatrix)
+# def customerMatrixiser():
+#     customerMatrix = []
+#     for i in range(0, len(customers)):
+#         sims = []
+#         for j in range(0,len(customers)):
+#             sims.append(customerSim(customers[i], customers[j]))
+#         customerMatrix.append(sims)
+#     return np.array(customerMatrix)
 
 def nearestNeighbors(customer, num, customerMatrix):
     index = customersMap[customer]

lib/products.py
@@ -1,7 +1,7 @@
-import customers as c
-import numpy     as np
+# import customers as c
+# import numpy     as np
 # from   scipy import sparse
-import math
+# import math
 
 productMatrix = {}
 productFreq = {}
@@ -15,62 +15,48 @@ def addProducts(prods):
     for i in range(0, len(prods)):
         productsMap[prods[i]] = i
 
-# def checkProducts(shoppers):
-#     sparseMatrix = sparse.csr_matrix(c.matrix)
+# # def checkProducts(shoppers):
+# #     sparseMatrix = sparse.csr_matrix(c.matrix)
 
-def productSim(item1, item2):
-    matrix = c.matrix
-    one = productsMap[item1]
-    two = productsMap[item2]
+# def productSim(item1, item2):
+#     matrix = c.matrix
+#     one = productsMap[item1]
+#     two = productsMap[item2]
 
-    intersection = 0.0
-    union = 0.0
-    for i in range(0,len(matrix)):
-        if matrix[i][one] > 0 and matrix[i][two] > 0:
-            union += 1
-            intersection += 1
-        elif matrix[i][one] > 0 or matrix[i][two] > 0:
-            union += 1
-    return 1.0 - math.tanh(math.sqrt(union - intersection))
-    # intersection = 0.0
-    # chances = 0.0
-    # if one is not 'null' and two is not 'null':
-    #     for i in range(0,len(one)):
-    #         for j in range(0, len(two)):
-    #             if one[i] == two[j]:
-    #                 intersection += 1
-    #     if(len(one) < len(two)):
-    #         chances += len(one)
-    #     else:
-    #         chances += len(two)
-    #     return 1.0 - math.tanh(math.sqrt(chances - intersection))
-    # else:
-    #     return 'null'
+#     intersection = 0.0
+#     union = 0.0
+#     for i in range(0,len(matrix)):
+#         if matrix[i][one] > 0 and matrix[i][two] > 0:
+#             union += 1
+#             intersection += 1
+#         elif matrix[i][one] > 0 or matrix[i][two] > 0:
+#             union += 1
+#     return 1.0 - math.tanh(math.sqrt(union - intersection))
 
-def productMatrixiser():
-    productMatrix = []
-    # checkProducts(c.customers)
-    for i in range(0, len(products)):
-        sims = []
-        for j in range(0, len(products)):
-            sims.append(productSim(products[i], products[j]))
-        productMatrix.append(sims)
-    return np.array(productMatrix)
+# def productMatrixiser():
+#     productMatrix = []
+#     # checkProducts(c.customers)
+#     for i in range(0, len(products)):
+#         sims = []
+#         for j in range(0, len(products)):
+#             sims.append(productSim(products[i], products[j]))
+#         productMatrix.append(sims)
+#     return np.array(productMatrix)
 
-def averageDistance(prodMatrix):
-    rowDist = 0.0
-    for i in range(0, len(prodMatrix)):
-        rowDist += np.sum(prodMatrix[i])/len(prodMatrix[i])
-    dist = rowDist/len(prodMatrix) 
-    return dist
+# def averageDistance(prodMatrix):
+#     rowDist = 0.0
+#     for i in range(0, len(prodMatrix)):
+#         rowDist += np.sum(prodMatrix[i])/len(prodMatrix[i])
+#     dist = rowDist/len(prodMatrix) 
+#     return dist
 
-def combineProducts(cluster, transpose):
-    reducedTranspose = []
-    for i in range(0, len(cluster)):
-        reducedRow = np.zeros((500))
-        for j in range(0, len(cluster[i])):
-            reducedRow = np.add(reducedRow, transpose[productsMap[cluster[i][j]]])
-        reducedTranspose.append(reducedRow)
-    numpyTranspose = np.array(reducedTranspose)
-    global matrix
-    matrix = numpyTranspose.transpose()
+# def combineProducts(cluster, transpose):
+#     reducedTranspose = []
+#     for i in range(0, len(cluster)):
+#         reducedRow = np.zeros((500))
+#         for j in range(0, len(cluster[i])):
+#             reducedRow = np.add(reducedRow, transpose[productsMap[cluster[i][j]]])
+#         reducedTranspose.append(reducedRow)
+#     numpyTranspose = np.array(reducedTranspose)
+#     global matrix
+#     matrix = numpyTranspose.transpose()

lib/util.py
@@ -38,21 +38,21 @@ def binarySearch(item, arr, low=0, high=-1):
     else:
         return (low+high)/2
 
-def dupCheck(clusters):
-    check = {}
-    for i in range(0,len(clusters)):
-        for j in range(0,len(clusters[i])):
-            res = check.get(clusters[i][j], [0,-1])
-            res[0] += 1
-            res[1] = i
-            check[clusters[i][j]] = res
-    prods = check.keys()
-    for i in range(0,len(prods)):
-        if(check[prods[i]][0] > 1):
-            print 'failure: ' + prods[i] + ' -- ' + str(check[prods[i]])
+# def dupCheck(clusters):
+#     check = {}
+#     for i in range(0,len(clusters)):
+#         for j in range(0,len(clusters[i])):
+#             res = check.get(clusters[i][j], [0,-1])
+#             res[0] += 1
+#             res[1] = i
+#             check[clusters[i][j]] = res
+#     prods = check.keys()
+#     for i in range(0,len(prods)):
+#         if(check[prods[i]][0] > 1):
+#             print 'failure: ' + prods[i] + ' -- ' + str(check[prods[i]])
 
 def dist(v1, v2):
-    comb = (v1 + v2)**2.
+    comb = (v1 - v2)**2.
     distance = np.sum(comb)**(1./2)
     return distance
 