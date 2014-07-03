23adadf4acc6213ed1495d02dd2b514e367e9514
lramsey <lramsey177@gmail.com>
2014-07-02 21:46:07 -0700

    refactor silhouette logic

--- lib/silhouette.py
+++ lib/silhouette.py
@@ -1,4 +1,5 @@
 import customers  as c
+import util       as u
 
 matrix = [[]]
 silhouettesList = []
@@ -7,17 +8,15 @@ centroids = []
 def __init__(mat, cents):
     global matrix
     matrix = mat
-    global silhouettesList
-    silhouettesList = []
     global centroids
     centroids = cents
 
 def distFromCentroid(cluster, centroid):
     a = 0.0
     for i in range(0, len(cluster)):
-        vector = matrix[c.customersMap[cluster[i].name]]
-        dist = (matrix[c.customersMap[cluster[0].name]] - centroid)**2.0
-        a += dist.sum()/len(vector)
+        point = customerPoint(cluster[i])
+        dist = u.dist(point, centroid)
+        a += dist
     avg = a/len(cluster)
     return avg
 
@@ -25,19 +24,35 @@ def averageSilhouettes(clusters, matrix, centroids):
     __init__(matrix, centroids)
     silhouettes = 0.0
     for i in range(0, len(clusters)):
-        s = silhouette(clusters[i], i)
+        center = centroids[i]
+        neighbor = neighboringCentroid(clusters[i], i)
+        s = 0.0
+        for j in range(0, len(clusters[i])):
+            point = customerPoint(clusters[i][j])
+            s += silhouette(point, center, neighbor)
+        clustSil = s/len(clusters[i])
+        silhouettesList.append(clustSil)
         silhouettes += s
-    return silhouettes/len(clusters)
+    return silhouettes/len(c.customers)
 
-def silhouette(cluster, index):
-    a = distFromCentroid(cluster, centroids[index])
-    b = a.size
-    for i in range(0, len(centroids)):
+def silhouette(point, centroid, neighbor):
+    a = u.dist(point, centroid)
+    b = u.dist(point, neighbor)
+    sil = (b-a)/max(a,b)
+    return sil
+
+def neighboringCentroid(cluster, index):
+    amin = len(customerPoint(cluster[0]))
+    neighborIndex = -.1
+    for i in range(0,len(centroids)):
         if i == index:
             continue
         dist = distFromCentroid(cluster, centroids[i])
-        if dist < b:
-            b = dist
-    sil = (b-a)/max(a,b)
-    silhouettesList.append(sil)
-    return sil
+        if dist < amin:
+            amin = dist
+            neighborIndex = i
+    neighbor = centroids[neighborIndex]
+    return neighbor
+
+def customerPoint(customer):
+    return matrix[c.customersMap[customer.name]]
